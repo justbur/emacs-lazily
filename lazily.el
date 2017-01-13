531a0f15811f8efa2520d75c723d8e8b8b8511f8
Justin Burkett <justin@burkett.cc>
2017-01-12 20:57:49 -0500

    Fix flycheck warnings

--- lazily.el
+++ lazily.el
@@ -21,33 +21,34 @@
 ;;; Commentary:
 
 ;; This package simply provides the macro `lazily-do' for use in Emacs
-;; configuration files. It only does one thing. You can wrap any configuration
+;; configuration files.  It only does one thing.  You can wrap any configuration
 ;; steps in the macro without having to worry about void variable messages or
-;; void variable messages. In the example below good-list is declared, but Emacs
-;; doesn't know about bad-list yet. This will throw a void-variable error.
+;; void variable messages.  In the example below good-list is declared, but
+;; Emacs doesn't know about bad-list yet.  This will throw a void-variable
+;; error.
 
 ;; (defvar good-list nil)
 ;; (lazily-do
 ;;  (add-to-list 'good-list 1)
 ;;  (add-to-list 'bad-list 1))
 
-;; We have a couple of options here. We could figure out which library defines
+;; We have a couple of options here.  We could figure out which library defines
 ;; bad-list and use `with-eval-after-load' like this.
 
 ;; (with-eval-after-load 'bad-library
 ;;  (add-to-list 'bad-list 1))
 
 ;; We could also explicitly require bad-library, which might be slow, or
-;; something else. The way `lazily-do' works is it stores the forms in a list
+;; something else.  The way `lazily-do' works is it stores the forms in a list
 ;; and tries again to eval those forms (in order) every time a new library is
-;; loaded. It accomplishes this through using the `after-load-functions' hook.
+;; loaded.  It accomplishes this through using the `after-load-functions' hook.
 
-;; To illustrate, suppose we had the original example in our config. The form
+;; To illustrate, suppose we had the original example in our config.  The form
 ;; with good-list will execute fine and there will be no difference to having
-;; this form at the top level. The one with bad-list throws a void-variable
-;; error which is caught and the form is saved for later. We do some work and
-;; eventually something calls or loads bad-library. This fires `lazily--redo' to
-;; try the saved forms again, and now that bad-list is defined the original
+;; this form at the top level.  The one with bad-list throws a void-variable
+;; error which is caught and the form is saved for later.  We do some work and
+;; eventually something calls or loads bad-library.  This fires `lazily--redo'
+;; to try the saved forms again, and now that bad-list is defined the original
 ;; error goes away and everything is dandy.
 
 ;;; Code:
@@ -55,6 +56,10 @@
 (defvar lazily--bad-forms nil)
 
 (defun lazily--redo (&rest _)
+  "Try to execute all forms in `lazily--bad-forms'.
+
+Any forms that throw void-variable or void-function errors are
+kept in `lazily--bad-forms' to be tried again later."
   (let (form still-bad)
     (while lazily--bad-forms
       (setq form (pop lazily--bad-forms))
@@ -67,6 +72,12 @@
       (setq lazily--bad-forms (nreverse still-bad)))))
 
 (defmacro lazily-do (&rest forms)
+  "Eval FORMS catching void-variable or void-function errors.
+
+Any forms that throw void-variable or void-function errors are
+stored in `lazily--bad-forms' to be tried again later.
+Specifically, a `after-load-functions' hook is added to try and
+execute these bad forms again after a new feature is loaded."
   `(let (error-forms)
      (dolist (form ',forms)
        (condition-case nil
