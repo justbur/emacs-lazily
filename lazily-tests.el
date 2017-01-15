3a80b7ae225805a605f43d7ed1f6a7f4a800f12e
Justin Burkett <justin@burkett.cc>
2017-01-14 23:00:10 -0500

    Fix tests for previous commit

--- lazily-tests.el
+++ lazily-tests.el
@@ -24,9 +24,77 @@
 (require 'lazily)
 (require 'ert)
 
-(ert-deftest lazily-tests-check-form-processing ()
-  "Check that forms are added to `lazily--bad-forms' correctly."
-  (let (lazily--bad-forms)
+(ert-deftest lazily-tests-check-do-all-form-processing ()
+  "Check that `lazily-do-all' uses `lazily--bad-forms' correctly."
+  (let (lazily--bad-forms after-load-functions)
+    (setq lazily-tests-good-list '(1))
+    (defun lazily-tests-good-func () nil)
+    (unintern 'lazily-tests-bad-list obarray)
+    (unintern 'lazily-tests-bad-func obarray)
+    ;; (message "0:\n%s" lazily--bad-forms)
+    (lazily-do-all
+     (add-to-list 'lazily-tests-good-list 2)
+     (add-to-list 'lazily-tests-bad-list 2)
+     (lazily-tests-good-func)
+     (lazily-tests-bad-func))
+    ;; (message "1:\n%s" lazily--bad-forms)
+    (should
+     (equal lazily--bad-forms
+            '((((add-to-list 'lazily-tests-bad-list 2)
+                (void-variable lazily-tests-bad-list)
+                nil))
+              (((lazily-tests-bad-func)
+                (void-function lazily-tests-bad-func)
+                nil)))))
+    (defun lazily-tests-bad-func () nil)
+    (lazily--redo)
+    ;; (message "2:\n%s" lazily--bad-forms)
+    (should
+     (equal lazily--bad-forms
+            '((((add-to-list 'lazily-tests-bad-list 2)
+                (void-variable lazily-tests-bad-list)
+                nil)))))
+    (setq lazily-tests-bad-list '(1))
+    (lazily--redo)
+    ;; (message "3:\n%s" lazily--bad-forms)
+    (should (null lazily--bad-forms))))
+
+(ert-deftest lazily-tests-check-do-form-processing ()
+  "Check that `lazily-do' uses `lazily--bad-forms' correctly."
+  (let (lazily--bad-forms after-load-functions)
+    (setq lazily-tests-good-list '(1))
+    (defun lazily-tests-good-func () nil)
+    (unintern 'lazily-tests-bad-list obarray)
+    (unintern 'lazily-tests-bad-func obarray)
+    (lazily-do
+     (add-to-list 'lazily-tests-good-list 2)
+     (add-to-list 'lazily-tests-bad-list 2)
+     (lazily-tests-good-func)
+     (lazily-tests-bad-func))
+    ;; (message "1:\n%s" lazily--bad-forms)
+    (should
+     (equal lazily--bad-forms
+            '((((add-to-list 'lazily-tests-bad-list 2)
+                (void-variable lazily-tests-bad-list) nil)
+               ((lazily-tests-good-func))
+               ((lazily-tests-bad-func))))))
+    (defun lazily-tests-bad-func () nil)
+    (lazily--redo)
+    ;; (message "2:\n%s" lazily--bad-forms)
+    (should
+     (equal lazily--bad-forms
+            '((((add-to-list 'lazily-tests-bad-list 2)
+                (void-variable lazily-tests-bad-list) nil)
+               ((lazily-tests-good-func))
+               ((lazily-tests-bad-func))))))
+    (setq lazily-tests-bad-list '(1))
+    (lazily--redo)
+    ;; (message "3:\n%s" lazily--bad-forms)
+    (should (null lazily--bad-forms))))
+
+(ert-deftest lazily-tests-check-do-form-processing-2 ()
+  "Check that `lazily-do' uses `lazily--bad-forms' correctly."
+  (let (lazily--bad-forms after-load-functions)
     (setq lazily-tests-good-list '(1))
     (defun lazily-tests-good-func () nil)
     (unintern 'lazily-tests-bad-list obarray)
@@ -38,16 +106,18 @@
      (lazily-tests-bad-func))
     (should
      (equal lazily--bad-forms
-            '(((add-to-list 'lazily-tests-bad-list 2)
-               (void-variable lazily-tests-bad-list) nil)
-              ((lazily-tests-bad-func)
-               (void-function lazily-tests-bad-func) nil))))
-    (defun lazily-tests-bad-func () nil)
+            '((((add-to-list 'lazily-tests-bad-list 2)
+                (void-variable lazily-tests-bad-list) nil)
+               ((lazily-tests-good-func))
+               ((lazily-tests-bad-func))))))
+    (setq lazily-tests-bad-list '(1))
     (lazily--redo)
+    ;; (message "2:\n%s" lazily--bad-forms)
     (should
      (equal lazily--bad-forms
-            '(((add-to-list 'lazily-tests-bad-list 2)
-               (void-variable lazily-tests-bad-list) nil))))
-    (setq lazily-tests-bad-list '(1))
+            '((((lazily-tests-bad-func)
+                (void-function lazily-tests-bad-func)
+                nil)))))
+    (defun lazily-tests-bad-func () nil)
     (lazily--redo)
     (should (null lazily--bad-forms))))
